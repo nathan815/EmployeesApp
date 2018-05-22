556278fd8c4a12d81306cffa1f12acd8bede0a17
Nathaniel Johnson <nathancj224@gmail.com>
2018-05-22 00:23:57 -0400

    Removed unused ModelInterface

--- src/Models/ModelInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Models;
-
-interface ModelInterface {
-    public static function fromState($state);
-}
--- src/autoload.php
+++ src/autoload.php
@@ -4,7 +4,7 @@ spl_autoload_register(function($class) {
     $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $classPath = str_replace('App', '', $classPath);
     $classPath = trim($classPath, DIRECTORY_SEPARATOR);
-    $file = BASE_PATH . 'src' . DIRECTORY_SEPARATOR . $classPath . '.php';
+    $file = SRC_PATH . $classPath . '.php';
     if(file_exists($file)) {
         require $file;
     }
